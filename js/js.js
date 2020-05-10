0cff29959d047b486b7b1a92d75ed66116ee2e6d
findelman <Solomonkr@mail.ru>
2020-05-11 00:52:04 +0600

    reborn

--- js/js.js
+++ js/js.js
@@ -1,16 +1,18 @@
-$(function(){
+$(function () {
 
-    $('.slide').slick({
-           infinite: false,
-           dots: true,
-           arrows: false,
-           dotsClass: 'sldotq',
-
-
-    
-
+    $('.sleq').slick({
+        infinite: false,
+        dots: true,
+        arrows: false,
+        dotsClass: 'sldotq',
     });
-
- 
+    $('.main-slide').slick({  
+        speed: 200,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 800,
+        dotsClass: 'nav-main_slide',
+    })
 })
-
